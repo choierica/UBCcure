b2de92e54f36a2a9484c7852941923f5082d9854
l1z1b <se110301@gmail.com>
2019-01-28 22:27:41 -0800

    Uploading to github, from shared to public

---
+++ README.md
@@ -0,0 +1,26 @@
+## Inspiration
+
+Vancouver is prone to a number of natural disasters, such as Wildfires, Earthquakes and Hailstorms. We wanted to build an alexa skill that allows families around the world to quiz themselves on safety procedures on a daily basis. 
+
+## What it does
+
+**UBCcure** is an Alexa skill, a "Safety Trivia" that allows users to interact with Alexa, answering questions randomized from a collection of different questions safety and gain points upon getting the right answer. 
+
+## How we built it
+
+We built UBCcure with javascript, on Amazon's very own _ AWS _developer console and _ Lambda _. We implemented Alexa Skills Kit SDK for Node.js. We have tested on Amazon's Echo 2. 
+
+## Challenges we ran into
+
+Fixing unexpected errors that arose in the middle of the night. 
+
+## Accomplishments that we're proud of
+
+Learning how to use and integrate AWS and Lambda in one day. It was our first Hackathon for most of us, and we had a lot of fun working with new technology. 
+
+## What we learned
+
+We learned that we can do anything, especially when we put our minds into it. Nothing is impossible!!!
+
+## What's next for UBCcure
+We are planning on making the code open-source with detailed instructions so that anyone around the world can easily access the code and make changes that best fit their needs.
---
+++ questions.js
@@ -0,0 +1,138 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_US: [
+    {
+      'During an earthquake drill which action should you NOT do?': [
+        'run from your shelter',
+        'hold on to your shelter',
+        'drop to your hands and knees',
+        'cover your head and neck',
+      ],
+    },
+    {
+      'If you are escaping from a building during a fire or other emergency, what should you do?': [
+        'Do not use the elevators or escape to the roof.',
+        'Open doors that feel hot.',
+        'Wait around to gather your belongings.',
+        'Walk towards slippery floors.',
+      ],
+    },
+    {
+      'After the earthquake, which of these courses of action will be of least importance?': [
+        'Visiting friends to chit-chat about the events of the day.',
+        'Helping people who need special assistance such as the disabled, children and the elderly.',
+        'Listening to a portable battery operated radio for emergency information and instructions.',
+        'Looking for and put out small fires.',
+      ],
+    },
+    {
+      'After an earthquake, which other natural disaster should persons, especially in coastal areas, expect and prepare for?': [
+        'tsunami',
+        'blizzard',
+        'hurricane',
+        'hailstorm',
+      ],
+    },
+    {
+      'You are in the kitchen cooking when you feel the first tremor. What should be your first response?': [
+        'Quickly turn off the stove and take cover.',
+        'Try to speed up the cooking by turning up the flame.',
+        'Run out of the kitchen.',
+        'Shout earthquake and try to find your spouse.',
+      ],
+    },
+    {
+      'During an earthquake drill which action should you NOT do?': [
+        'run from your shelter',
+        'hold on to your shelter',
+        'drop to your hands and knees',
+        'cover your head and neck',
+      ],
+    },
+    {
+      'If you are escaping from a building during a fire or other emergency?': [
+        'Open doors that feel hot.',
+        'Do not use the elevators or escape to the roof.',
+        'Wait around to gather your belongings.',
+        'Walk towards slippery floors.',
+      ],
+    },
+    {
+      'After the earthquake which of these courses of action will be of least importance?': [
+        'Visiting friends to chit-chat about the events of the day.',
+        'Helping people who need special assistance such as the disabled, children and the elderly.',
+        'Listening to a portable battery operated radio for emergency information and instructions.',
+        'Looking for and put out small fires.',
+      ],
+    },
+    {
+      'After an earthquake which other natural disaster should persons, especially in coastal areas, expect and prepare for?': [
+        'tsunami',
+        'blizzard',
+        'hurricane',
+        'hailstorm',
+      ],
+    },
+    {
+      'You are in the kitchen cooking when you feel the first tremor. What should be your first response?': [
+        'in the open',
+        'near the exterior walls',
+        'nowhere outside, run towards the door',
+        'in a sinkhole',
+      ],
+    },
+    {
+      'During an earthquake drill which action should you NOT do?': [
+        'run from your shelter',
+        'hold on to your shelter',
+        'drop to your hands and knees',
+        'cover your head and neck',
+      ],
+    },
+    {
+      'What is the MOST important Firearm Safety Rule?': [
+        'Keep your finger off the trigger.',
+        'Know the special rules for shooting at an indoor shooting range.',
+        'Use the correct ammunition for your firearm.',
+        'Always keep your firearm pointed in a safe direction.',
+      ],
+    },
+    {
+      'Which of the following is false?': [
+        'Always keep your gun unloaded until you are ready to use it.',
+        'Always keep your finger off the trigger until you are ready to shoot.',
+        'You are allowed to leave a loaded gun laying around as long as children are not present.',
+        'Make certain to use the correct ammunition for your firearm.',
+      ],
+    },
+    {
+      'Why is knowing your natural stress response key to surviving an active shooter event?': [
+        'Because you’ll recognize when your survival instinct is taking over your “thinking” brain',
+        'Because your stress response is your best guide to safety in case of an emergency',
+        'Because you’ll immediately be able to decide that running away from the shooter is your best response',
+        'Because you should always fight as a first response to an active shooter',
+      ],
+    },
+    {
+      'During an active shooter event, which of the following is an important factor that could help save your life?': [
+        'Situational awareness',
+        'Knowing the identity of the shooter',
+        'An awareness that active shooter events are on the rise',
+        'Classes in self-defense or martial arts',
+      ],
+    },
+    {
+      'What to do if there is an active shooter in your building?': [
+        'if you determine it is safe, run',
+        'take your belongings',
+        'Call 911 immediately',
+        'Hide in washroom',
+      ],
+    },
+  ],
+};
